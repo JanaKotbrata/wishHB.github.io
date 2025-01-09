8694575d8812cabee24d61759aca09b739c7dbe3
Jani <71846298+JanaKotbrata@users.noreply.github.com>
2025-01-09 23:46:37 +0100

    Create task-config.js
    added conf

---
+++ helpers/task-config.js
@@ -0,0 +1,44 @@
+export const taskList = [ //FIXME do it hidden
+  {
+    id: "task1",
+    class: "form-group",
+    h2: "Úkol 1: Kolik je 37 + 37?",
+    inputId: "answer1",
+    correctAnswer: "74",
+  },
+  {
+    id: "task2",
+    class: "form-group",
+    h2: "Úkol 2: Vyplň následující kód: function nani() { return '",
+    inputId: "answer2",
+    correctAnswer: ["láska';}", "love';}"],
+  },
+  {
+    id: "task3",
+    class: "form-group",
+    h2: "Úkol 3: Kde proběhla první pusa s Nani? (Město)?",
+    inputId: "answer3",
+    correctAnswer: "antverpy",
+  },
+  {
+    id: "task4",
+    class: "form-group",
+    h2: "Úkol 4: Rozlušti šifru 84-105-108-97-110",
+    inputId: "answer4",
+    correctAnswer: "milan",
+  },
+  {
+    id: "task5",
+    class: "form-group",
+    h2: "Úkol 5: Najdi další číslo v řadě 2, 4, 6, 10, 16 , 21, ?, 58, 95,...",
+    inputId: "answer5",
+    correctAnswer: "37",
+  },
+  {
+    id: "task6",
+    class: "form-group",
+    h2: "Úkol 6: Kolik cizích zemí jsme spolu navštívili?",
+    inputId: "answer6",
+    correctAnswer: "7",
+  },
+];
